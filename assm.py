fb0b0ac98cd520d074185a102ed8ffbdff5336c4
Precious Akpan <precyudy@gmail.com>
2023-01-18 13:38:16 +0100

    first commit

---
+++ assm.py
@@ -0,0 +1,6 @@
+name = input ("What is your name  ")
+friend = input("What is your friend's name   ")
+
+print ("Hello", name, "your friend's name is", friend)
+
+print (f"Hello {name} your friend's name is {friend}")
